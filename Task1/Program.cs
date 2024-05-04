027e0feacd9ec079d3faf1130a667da9dfabf045
Oleg Karlin <o.karlin1981@gmail.com>
2024-05-04 15:12:32 +0300

    add Lession3:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №1
+
+// Вычислить значение формулы a * b / c + d, где a, b, c, d - некоторые целые числа.
+// Решение оформить в виде функции.
+
+// Создание функции:
+// 1. Что делает функция?
+// 2. Какие данные ей нужны для работы?
+// 3. Что будет результатом работы?
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
